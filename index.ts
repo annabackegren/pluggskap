edffd854382a4fa736bb19f51d2206dcc745170c
annabackegren <anna.backegren@gmail.com>
2026-02-17 15:38:39 +0100

    fixat tsconfig

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
-function add(a: number, b: number): number{
-    return a + b
-}
-console.log(add(2, 3))
+import express from 'express'
+
+const app = express()
+
+app.get('/', (_request, response) => {
+  response.send('Hello World!')
+})
+
+app.listen(3000, () => {
+  console.log('Webbtjänsten kan nu ta emot anrop.')
+})

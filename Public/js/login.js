ceab8ea10f5b00f5aa32901f58cbef4962bcd67c
jimsfs <jimmiesvardh@live.se>
2026-02-26 12:11:26 +0100

    Funktioner för att logga in och registera

---
+++ Public/js/login.js
@@ -0,0 +1,58 @@
+const url = "http://localhost:3000/user/login";
+
+const form = document.querySelector("#loginform");
+const loginError = document.querySelector("#loginerror");
+
+loginError.style.display = "none";
+
+function clearInputs() {
+  const username = document.querySelector("#username");
+  const password = document.querySelector("#password");
+
+  username.value = "";
+  password.value = "";
+}
+
+clearInputs();
+
+async function loginReq() {
+  try {
+    const username = document.querySelector("#username");
+    const password = document.querySelector("#password");
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        userName: username.value,
+        userPassword: password.value,
+      }),
+    });
+    console.log(await response.json());
+    if ((await response.status) === 200) {
+      username.value = "";
+      password.value = "";
+      window.location.href = "http://localhost:3000/html/homepage.html";
+    } else {
+      console.log("fel");
+      loginError.style.display = "block";
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getUserId() {
+  try {
+    const response = await fetch("http://localhost:3000/user");
+    const result = response.json();
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  loginReq();
+  getUserId();
+});
---
+++ Public/js/register.js
@@ -0,0 +1,98 @@
+const url = "http://localhost:3000/user/";
+
+const form = document.querySelector("#userinfo");
+const submitbtn = document.querySelector("#submitbtn");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirmpassword");
+const gdprcheck = document.querySelector("#gdprcheck");
+const passwordError = document.querySelector("#passworderror");
+const cfpasswordError = document.querySelector("#cfpassworderror");
+const userCreated = document.querySelector("#usercreated");
+const loginBtn = document.querySelector("#loginbtn");
+
+loginBtn.style.display = "none";
+userCreated.style.display = "none";
+cfpasswordError.style.display = "none";
+passwordError.style.display = "none";
+submitbtn.disabled = true;
+
+function clearInputs() {
+  const larareBtn = document.querySelector("#lararebtn");
+  const elevBtn = document.querySelector("#elevbtn");
+  const gdprcheck = document.querySelector("#gdprcheck");
+  const username = document.querySelector("#username");
+
+  username.value = "";
+  larareBtn.checked = false;
+  elevBtn.checked = false;
+  gdprcheck.checked = false;
+}
+
+clearInputs();
+
+function checkForm() {
+  if (
+    password.value.length > 3 &&
+    confirmPassword.value.length > 3 &&
+    password.value === confirmPassword.value &&
+    gdprcheck.checked === true
+  ) {
+    submitbtn.disabled = false;
+  } else {
+    submitbtn.disabled = true;
+  }
+}
+
+function errorForm() {
+  if (password.value.length > 3) {
+    passwordError.style.display = "none";
+  } else {
+    passwordError.style.display = "block";
+  }
+  if (confirmPassword.value === password.value || confirmPassword.value < 1) {
+    cfpasswordError.style.display = "none";
+  } else {
+    cfpasswordError.style.display = "block";
+  }
+}
+
+gdprcheck.addEventListener("change", checkForm);
+
+password.addEventListener("keyup", () => {
+  checkForm();
+  errorForm();
+});
+
+confirmPassword.addEventListener("keyup", () => {
+  checkForm();
+  errorForm();
+});
+
+async function sendData() {
+  try {
+    const usertype = document.querySelector(".usertype:checked");
+    const username = document.querySelector("#username");
+    const password = document.querySelector("#password");
+    const confirmPassword = document.querySelector("#confirmpassword");
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        userType: usertype.value,
+        userName: username.value,
+        userPassword: confirmPassword.value,
+      }),
+    });
+    console.log(await response.json());
+    form.style.display = "none";
+    userCreated.style.display = "block";
+    loginBtn.style.display = "block";
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  sendData();
+});

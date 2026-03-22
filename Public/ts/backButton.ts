1a8ca0d1503003b80d97f68aa0a6246abb2dff8e
omashi94 <omariomer@hotmail.com>
2026-03-22 23:13:53 +0100

    fixade backbutton så att quiz tar användaren till map.html

--- Public/ts/backButton.ts
+++ Public/ts/backButton.ts
@@ -2,6 +2,10 @@ const backBtn = document.querySelector<HTMLButtonElement>("#back-button");
 
 if (backBtn) {
   backBtn.addEventListener("click", function (): void {
-    window.history.back();
+    if (window.location.pathname.includes("quiz.html")) {
+      window.location.href = "../HTML/map.html";
+    } else {
+      window.history.back();
+    }
   });
 }

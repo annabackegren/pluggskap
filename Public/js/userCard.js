b229f139ef852104e45668f18e29bc7940d58f0a
annabackegren <anna.backegren@gmail.com>
2026-03-23 10:30:40 +0100

    kompilering userCard

--- Public/js/userCard.js
+++ Public/js/userCard.js
@@ -1,35 +1,70 @@
-async function user() {
-  const userEl = document.querySelector(".user-card");
-
-  if (userEl) {
-    const result = await fetch("/html/userCard.html"),
-      user = await result.text();
-    userEl.innerHTML = user;
-
-    let showUsername = JSON.parse(localStorage.getItem("usernameIndex"));
-    const usernameEl = document.getElementById("user-name");
-
-    if (usernameEl)
-      usernameEl.textContent = showUsername || "Förnamn Efternamn";
-
-    try {
-      const response = await fetch("/user/" + showUsername);
-      const userData = await response.json();
-      const profileImg = userEl.querySelector("img");
-
-      if (profileImg && userData.userType === "larare") {
-        profileImg.src = "../assets/img/misc/dancing.png";
-      }
-    } catch (error) {
-      console.error(error);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-  }
-
-  const userCardBox = document.querySelector("#user-card-box");
-  const changeUserInfo = document.querySelector("#change-user-info");
-
-  if (userCardBox && changeUserInfo) {
-    userCardBox.appendChild(changeUserInfo);
-  }
+};
+function user() {
+    return __awaiter(this, void 0, void 0, function () {
+        var userEl, result, user_1, showUserName, usernameEl, userCardBox, changeUserInfo;
+        var _a;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    userEl = document.querySelector(".user-card");
+                    if (!userEl) return [3 /*break*/, 3];
+                    return [4 /*yield*/, fetch("../HTML/userCard.html")];
+                case 1:
+                    result = _b.sent();
+                    return [4 /*yield*/, result.text()];
+                case 2:
+                    user_1 = _b.sent();
+                    userEl.innerHTML = user_1;
+                    showUserName = JSON.parse((_a = localStorage.getItem("usernameIndex")) !== null && _a !== void 0 ? _a : '""');
+                    usernameEl = document.getElementById("user-name");
+                    if (usernameEl) {
+                        usernameEl.textContent = showUserName || "Förnamn Efternamn";
+                    }
+                    _b.label = 3;
+                case 3:
+                    userCardBox = document.querySelector("#user-card-box");
+                    changeUserInfo = document.querySelector("#change-user-info");
+                    if (userCardBox && changeUserInfo) {
+                        userCardBox.appendChild(changeUserInfo);
+                    }
+                    return [2 /*return*/];
+            }
+        });
+    });
 }
 user();
